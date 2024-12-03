3949f46a618006abf6408705283789a6d50c37ee
harjotsk03 <harjotsk03@gmail.com>
2024-12-03 11:02:25 -0800

    add finished ocean action grants page

---
+++ oceanactionrelated.js
@@ -0,0 +1,63 @@
+const popularGrantCards = [
+  {
+    id: 1,
+    title: "Eco-Action Accelerator",
+    categories: "Categories: Youth, Climate, Community",
+    link: "pages/EcoActionAccelerator.html",
+    amount: "$5000",
+    duration: "6 months",
+    deadline: "August 5",
+    amountTitle: "Amount",
+    durationTitle: "Duration",
+    deadlineTitle: "Deadline",
+  },
+  {
+    id: 2,
+    title: "Canadian Youth Climate Action Award",
+    link: "pages/CanadianYouthClimateActionAward.html",
+    amount: "$500-$5000",
+    deadline: "October 1",
+    amountTitle: "Amount",
+    deadlineTitle: "Deadline",
+  },
+];
+
+function createCard(data) {
+  return `
+                <div class="card" data-id="${data.id}">
+                  <div class="grantCardHeroContainer">  
+                    <div>
+                      <h3 class="cardTitle">${data.title}</h3>
+                      <p class="cardCategories">${data.categories ?? ""}</p>
+                    </div>
+                    <a class="cardViewButton largeGrantButton" href="${
+                      data.link
+                    }">View Grant Information</a>
+                  </div>
+                  <div class="grantCardInfoContainer">
+                    <div class="cardInfoContainer">
+                      <h4 class="classLabel">${data.amountTitle ?? ""}</h4>
+                      <p class="classInfo">${data.amount ?? ""}</p>
+                    </div>
+                    <div class="cardInfoContainer">
+                      <h4 class="classLabel">${data.durationTitle ?? ""}</h4>
+                      <p class="classInfo">${data.duration ?? ""}</p>
+                    </div>
+                    <div class="cardInfoContainer">
+                      <h4 class="classLabel">${data.deadlineTitle ?? ""}</h4>
+                      <p class="classInfo">${data.deadline ?? ""}</p>
+                    </div>
+                    <a class="cardViewButton smallGrantButton" href="${
+                      data.link
+                    }">View Grant Information</a>
+                  </div>
+                </div>
+            `;
+}
+
+function renderCards() {
+  const container = document.getElementById("oceanActionRelated");
+  const cardsHTML = popularGrantCards.map((card) => createCard(card)).join("");
+  container.innerHTML = cardsHTML;
+}
+renderCards();
